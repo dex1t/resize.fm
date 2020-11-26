559a3c1e3617df5ef3550267ff86a349de5f3430
dex1t <dex1t@deg.ooo>
2020-11-26 14:13:45 +0900

    Render episode index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { frontMatter as docsPages } from "./ep/*.mdx";
 
 export default function Home() {
   return (
@@ -12,6 +14,15 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>resize.fm</h1>
       </main>
+      <ul>
+        {docsPages.map((page) => (
+          <li key={page.__resourcePath}>
+            <Link href={page.__resourcePath.replace(/\.mdx$/, "")}>
+              <a>{page.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
