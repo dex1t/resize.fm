76059332a54ed582ba81c1699926bec0a9c09f77
dex1t <dex1t@deg.ooo>
2020-11-26 14:11:30 +0900

    Add Layout

---
+++ components/layouts/Episode.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Episode({ children, frontMatter }) {
+  return (
+    <article>
+      <Head>
+        <title>{frontMatter.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p>{frontMatter.anchor}</p>
+      <h1>{frontMatter.title}</h1>
+      {children}
+    </article>
+  );
+}
