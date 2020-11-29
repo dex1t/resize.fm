625035c8abbb497a417ee007af45f8bfbfbe0099
dex1t <dex1t@deg.ooo>
2020-11-29 19:58:59 +0900

    Apply prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,23 +13,41 @@ export default function Home() {
       <Head>
         <title>resize.fm</title>
         <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#F7F7F7"/>
-        <meta name="keywords" content="resizefm, podcast, リサイズ, エフエム, ポッドキャスト, design, service"/>
-        <meta name="description" content="元山（@kudakurage）と出口（@dex1t）が最近気になっているサービスやデザイントピックスを取り上げてのんびり話すPodcastです。 #resizefm"/>
-        <meta property="og:site_name" content="resize.fm"/>
-        <meta property="og:url" content="https://resize.fm/"/>
-        <meta property="og:title" content="resize.fm"/>
-        <meta property="og:description" content="元山（@kudakurage）と出口（@dex1t）が最近気になっているサービスやデザイントピックスを取り上げてのんびり話すPodcastです。 #resizefm"/>
-        <meta property="og:image" content="https://resize.fm/images/resizefm_ogp.png"/>
-        <meta property="og:image:width" content="1200"/>
-        <meta property="og:image:height" content="630"/>
-        <meta property="og:type" content="website"/>
-        <meta name="twitter:card" content="summary"/>
-        <meta name="twitter:site" content="@resizefm"/>
-        <meta name="twitter:url" content="https://resize.fm/"/>
-        <meta name="twitter:title" content="resize.fm"/>
-        <meta name="twitter:description" content="元山（@kudakurage）と出口（@dex1t）が最近気になっているサービスやデザイントピックスを取り上げてのんびり話すPodcastです。 #resizefm"/>
-        <meta name="twitter:image" content="https://resize.fm/images/resizefm_ogp.png"/>
+        <meta name="theme-color" content="#F7F7F7" />
+        <meta
+          name="keywords"
+          content="resizefm, podcast, リサイズ, エフエム, ポッドキャスト, design, service"
+        />
+        <meta
+          name="description"
+          content="元山（@kudakurage）と出口（@dex1t）が最近気になっているサービスやデザイントピックスを取り上げてのんびり話すPodcastです。 #resizefm"
+        />
+        <meta property="og:site_name" content="resize.fm" />
+        <meta property="og:url" content="https://resize.fm/" />
+        <meta property="og:title" content="resize.fm" />
+        <meta
+          property="og:description"
+          content="元山（@kudakurage）と出口（@dex1t）が最近気になっているサービスやデザイントピックスを取り上げてのんびり話すPodcastです。 #resizefm"
+        />
+        <meta
+          property="og:image"
+          content="https://resize.fm/images/resizefm_ogp.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@resizefm" />
+        <meta name="twitter:url" content="https://resize.fm/" />
+        <meta name="twitter:title" content="resize.fm" />
+        <meta
+          name="twitter:description"
+          content="元山（@kudakurage）と出口（@dex1t）が最近気になっているサービスやデザイントピックスを取り上げてのんびり話すPodcastです。 #resizefm"
+        />
+        <meta
+          name="twitter:image"
+          content="https://resize.fm/images/resizefm_ogp.png"
+        />
       </Head>
 
       <Header />
@@ -50,8 +68,14 @@ export default function Home() {
                 <div className={styles.title}>{page.title}</div>
                 <div className={styles.description}>{page.description}</div>
                 <div className={styles.meta}>
-                  <span className={styles.date}><img src="/images/calendar.svg" width="12"/>{page.date}</span>
-                  <span className={styles.time}><img src="/images/play.svg" width="12"/>{page.time}</span>
+                  <span className={styles.date}>
+                    <img src="/images/calendar.svg" width="12" />
+                    {page.date}
+                  </span>
+                  <span className={styles.time}>
+                    <img src="/images/play.svg" width="12" />
+                    {page.time}
+                  </span>
                 </div>
               </a>
             </Link>
