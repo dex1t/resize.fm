8f093e679f8d07390ed4c32602b8441828bb58ec
dex1t <dex1t@deg.ooo>
2022-09-22 19:50:20 +0900

    Disable manual tracking for GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,10 @@
 import { useEffect } from "react";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
 import "../styles/globals.scss";
 import AppLayout from "../components/layouts/AppLayout";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <AppLayout>
       <Component {...pageProps} />
