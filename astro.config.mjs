bd2d5e2ab3d8f97bf48a392aae59ee2dd3dd0c6e
dex1t <dex1t@5m.design>
2023-10-28 21:29:43 +0900

    Inject GA Tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/ep": "/"
+    "/ep": "/",
   },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "src/styles/_mixin.scss", "src/styles/_tokens.scss";`
-        }
-      }
-    }
+          additionalData: `@import "src/styles/_mixin.scss", "src/styles/_tokens.scss";`,
+        },
+      },
+    },
   },
-  integrations: [react()]
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
