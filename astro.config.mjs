9ea9bda198d960e770a59452b7ccb651a067723d
dex1t <dex1t@5m.design>
2023-10-28 21:03:12 +0900

    Install react adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/ep": "/",
+    "/ep": "/"
   },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "src/styles/_mixin.scss", "src/styles/_tokens.scss";`,
-        },
-      },
-    },
+          additionalData: `@import "src/styles/_mixin.scss", "src/styles/_tokens.scss";`
+        }
+      }
+    }
   },
+  integrations: [react()]
 });
