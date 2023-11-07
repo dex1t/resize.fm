832d37b7eed4eaaa435e277fa4fdc8ede9cc4d4a
dex1t <dex1t@5m.design>
2023-11-08 00:38:04 +0900

    Fix schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection } from "astro:content";
 const episodeCollection = defineCollection({
   type: "content",
   schema: z.object({
-    src: z.string().url().optional(),
+    src: z.string().nullish(),
     title: z.string(),
     description: z.string(),
     date: z.string(),
